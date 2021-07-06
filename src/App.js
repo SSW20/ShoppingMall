c3794d3c5b111456d8ff333d019971236a276104
이름 <76836826+SSW20@users.noreply.github.com>
2021-07-06 16:11:39 +0900

    스토리보드 테스트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Event from "./Components/Event/Event";
+import Category from "./Components/Category/Category";
+import Selling from "./Components/Selling/Selling";
+import Basket from "./Components/Basket/Basket";
+import Support from "./Components/Support/Support";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+
+      <Switch>
+        <Route exact path="/Home" component={Home} />
+        <Route path="/event" component={Event} />
+        <Route path="/category" component={Category} />
+        <Route path="/selling" component={Selling} />
+        <Route path="/basket" component={Basket} />
+        <Route path="/support" component={Support} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/Components/Selling/Selling.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Selling() {
+  return (
+    <div>
+      <h1>판매</h1>
+    </div>
+  );
+}
+
+export default Selling;
